6d12c30552c3edf87bbaa612d5414a261dc58e50
55pp <kruvpky@mail.ru>
2019-07-22 15:57:35 +0700

    hhr

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,8 +1,17 @@
 package com;
 
+import java.net.InetAddress;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Client d");
+        InetAddress IP= null;
+        try {
+            IP = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        System.out.println("IP of my system is := "+IP.getHostAddress());
         Dispatcher dispatcher = new Dispatcher();
         NIOSocketServer server = new NIOSocketServer(dispatcher);
         while(true){
